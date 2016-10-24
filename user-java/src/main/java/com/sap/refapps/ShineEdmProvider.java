d4ab6a8134c0c53fa794a43a2282d8ea4d2c7f28
Kumar <C5208615@global.corp.sap>
2016-10-24 09:25:10 +0530

    Refactored some of the code as per code review changes

--- user-java/src/main/java/com/sap/refapps/ShineEdmProvider.java
+++ user-java/src/main/java/com/sap/refapps/ShineEdmProvider.java
@@ -2,10 +2,10 @@ package com.sap.refapps;
 
 import com.sap.gateway.v4.rt.xsa.cds.XSACDSEdmProvider;
 
-public class MyEdmProvider extends XSACDSEdmProvider{
+public class ShineEdmProvider extends XSACDSEdmProvider{
 
 		
-	public MyEdmProvider() {
+	public ShineEdmProvider() {
 		super();
 	}
 
