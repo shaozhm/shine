5623f78ee45eed8693b75df0eb5698a24b2e665f
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-07-13 08:11:03 +0530

    removing ws lib

--- core-js/router/index.js
+++ core-js/router/index.js
@@ -4,7 +4,7 @@ module.exports = function(app,server) {
 	 app.use("/node", require("./routes/myNode")());
 	 app.use("/node/excAsync", require("./routes/exerciseAsync")(server));
 	 app.use("/node/textBundle", require("./routes/textBundle")());
-	 app.use("/node/chat", require("./routes/chatServer")(server));
+	 //app.use("/node/chat", require("./routes/chatServer")(server));
 	 app.use("/node/excel", require("./routes/excel")());
 	 app.use("/node/xml", require("./routes/xml")());
 	 app.use("/node/zip", require("./routes/zip")());
