32a6179437a1704041742c2f7870b62faf011523
Kiruthika N <kiruthika.n@sap.com>
2017-08-17 14:18:34 +0530

    code push down

--- web/resources/admin-ui/view/default.controller.js
+++ web/resources/admin-ui/view/default.controller.js
@@ -127,7 +127,7 @@ sap.ui.controller("sap.hana.democontent.epm.admin.view.default", {
 		var totalPO = parseInt(oModel.getProperty('/POVal'), 10);
 		var totalSO = parseInt(oModel.getProperty('/SOVal'), 10);
 		
-		if(totalPO > 5 || totalSO > 5)
+		if(totalPO > 20 || totalSO > 20)
 		{   canContinue = false;
 			sap.ui.commons.MessageBox.show("Please enter a number between 1 and 5",sap.ui.commons.MessageBox.Icon.ERROR,"Error");
 			
@@ -230,38 +230,14 @@ sap.ui.controller("sap.hana.democontent.epm.admin.view.default", {
 				oModel.setProperty('/percentValue', 0);
 				oModel.setProperty('/displayValue', "");
 				oModel.setProperty('/txtLog', "");
-				var xsrf_token;
 				soLoops = 0;
 				poLoops = 0;
 				soRequired = parseInt(oModel.getProperty('/SOVal'), 10);;
 				poRequired = parseInt(oModel.getProperty('/POVal'), 10);;
-				$.ajax({
-						type: "GET",
-						async: false,
-						url: "/sap/hana/democontent/epm/services/poCreate.xsodata",
-						contentType: "application/json",
-						headers: {
-							'x-csrf-token': 'Fetch',
-							'Accept': "application/json"
-						},
-						success: function(data, textStatus, request) {
-							xsrf_token = request.getResponseHeader('x-csrf-token');
-						},
-						error: function(jqXHR, textStatus, errorThrown) {
-							sap.ui.commons.MessageBox.show("Error in fetching XSRF token",
-								"ERROR",
-								"Error");
-							return;
-						}
-					});
 				for (var i = 0; i < soRequired; i++) {
 					jQuery.ajax({
 						url: '/replicate/sales',
-						headers: {
-								'x-csrf-token': xsrf_token
-								 },
-						method: 'POST',
-						contentType: "application/json",
+						method: 'GET',
 						dataType: 'json',
 						success: function(myTxt) {
 							oModel.setProperty('/txtLog', myTxt.message + "\n" + oModel.getProperty('/txtLog'));
@@ -287,10 +263,7 @@ sap.ui.controller("sap.hana.democontent.epm.admin.view.default", {
 				for (var j = 0; j < poRequired; j++) {
 					jQuery.ajax({
 						url: '/replicate/purchase',
-						headers: {
-									'x-csrf-token': xsrf_token
-								 },
-						method: 'POST',
+						method: 'GET',
 						dataType: 'json',
 						success: function(myTxt) {
 							oModel.setProperty('/txtLog', myTxt.message + "\n" + oModel.getProperty('/txtLog'));
@@ -351,36 +324,11 @@ sap.ui.controller("sap.hana.democontent.epm.admin.view.default", {
 		oModel.setProperty('/displayValue', "");
 		oModel.setProperty('/txtLog', "");
 		resetCount = 0;
-		var xsrf_token;
 		resetMax = urls.length;
-		//get xsrf token
-			$.ajax({
-			type: "GET",
-			async: false,
-			url: "/sap/hana/democontent/epm/services/poCreate.xsodata",
-			contentType: "application/json",
-			headers: {
-				'x-csrf-token': 'Fetch',
-				'Accept': "application/json"
-			},
-			success: function(data, textStatus, request) {
-				xsrf_token = request.getResponseHeader('x-csrf-token');
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				sap.ui.commons.MessageBox.show("Error in fetching XSRF token",
-					"ERROR",
-					"Error");
-				return;
-			}
-		});
 		for (var i = 0; i < urls.length; i++) {
-			
 			jQuery.ajax({
 				url: urls[i],
-				headers: {
-						'x-csrf-token': xsrf_token
-			             },
-				method: 'POST',
+				method: 'GET',
 				dataType: 'json',
 				success: function(myTxt) {
 					oModel.setProperty('/txtLog', myTxt.message + "\n" + oModel.getProperty('/txtLog'));
@@ -450,14 +398,15 @@ sap.ui.controller("sap.hana.democontent.epm.admin.view.default", {
 			},
 			method: 'POST',
 			data: JSON.stringify(item),
-			async: true,
+			timeout: 0,
+			async: false,
 			contentType: "application/json",
 			dataType: 'json',
 			success: function(myTxt) {
 				var oModel = sap.ui.getCore().getModel();
 				//var total = oModel.getProperty('/percentValue');
 				oModel.setProperty('/txtLog', myTxt.message + "\n" + oModel.getProperty('/txtLog'));
-
+				oController.getTableSizes();
 				var totalPO = (parseInt(oModel.getProperty('/POVal'), 10)) * 1000;
 				var totalSO = (parseInt(oModel.getProperty('/SOVal'), 10)) * 1000;
 				var percentValue = totalPO + totalSO;
