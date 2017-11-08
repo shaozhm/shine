f0ae616be24a95f7726bb0d2aad25b3e3ef03f28
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-11-08 17:53:32 +0530

    converting JS view to XML view

--- web/resources/launchpad/view/main.controller.js
+++ web/resources/launchpad/view/main.controller.js
@@ -12,8 +12,46 @@ sap.ui.controller("view.main", {
 			var welcomeDialog = new sap.account.WelcomeDialog(this);
 			welcomeDialog.open();
 		}
-	}
+	},
+	handlePress: function(oEvent) {
+			var tileID;
+		    var tileId = oEvent.getSource().getId();
+		    if(tileId==="__xmlview0--dg"){
+		    	tileID=1;
+		    }
+		     if(tileId==="__xmlview0--po"){
+		    	tileID=2;
+		    }
+		     if(tileId==="__xmlview0--jobscheduling"){
+		    	tileID=3;
+		    }
+		     if(tileId==="__xmlview0--so"){
+		    	tileID=4;
+		    }
+		     if(tileId==="__xmlview0--uc"){
+		    	tileID=5;
+		    }
+		     if(tileId==="__xmlview0--spatial"){
+		    	tileID=8;
+		    }
+		    if(tileId==="__xmlview0--fulltextsearch"){
+		    	tileID=1;
+		    }
+		    
+		var value = sap.app.localStorage.getPreference(sap.app.localStorage.PREF_TILE_HELP_SHOW_PREFIX + tileID);
+        if (value !== 'false') {
+            var tileDialog = new sap.account.TileDialog(oController);
+            tileDialog.open(tileID);
+        } else {
+            var tileDialog = new sap.account.TileDialog(oController);
+            window.location = tileDialog.getHrefLocation(tileID);
+        }
+		},
 
+
+sourceCodeDownload: function(){
+	 sap.m.URLHelper.redirect("../source/sap-xsac-shine-src-code.zip", true);
+}
 	
 
 });
