1eb9984a4356ea8f2392d50d19f47c9f6e8d4837
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-11-23 10:15:03 +0530

    code standard checks and corrections

--- user-js/router/index.js
+++ user-js/router/index.js
@@ -1,7 +1,7 @@
-"use strict";
-
+/* eslint no-undef:0,no-unused-vars: 0*/
+'use strict';
 module.exports = function(app,server) {
-	app.use("/jobactivity", require("./routes/jobactivity")());
-	app.use("/jobs", require("./routes/jobs")());
-	app.use("/schedules", require("./routes/schedules")());	
+	app.use('/jobactivity', require('./routes/jobactivity')());
+	app.use('/jobs', require('./routes/jobs')());
+	app.use('/schedules', require('./routes/schedules')());
 };
