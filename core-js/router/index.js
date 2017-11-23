58344baa3f82a2441fb88ec612fddca269960405
i302582 <kiruthika.n@sap.com>
2017-11-23 09:48:44 +0000

    Fix lint errors

--- core-js/router/index.js
+++ core-js/router/index.js
@@ -1,7 +1,6 @@
 /* eslint no-undef:0,no-unused-vars: 0*/
 'use strict';
 
-
 module.exports = function(app,server) {
 	app.use('/node', require('./routes/myNode')());
 	app.use('/node/excAsync', require('./routes/exerciseAsync')(server));
