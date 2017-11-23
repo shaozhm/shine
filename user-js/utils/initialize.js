632e2281df1d62b19620e4717018acee7485775f
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-11-23 10:14:27 +0530

    code standard checks and corrections

--- user-js/utils/initialize.js
+++ user-js/utils/initialize.js
@@ -1,60 +1,59 @@
-/*eslint no-console: 0, no-unused-vars: 0*/
-"use strict";
+/*eslint no-console: 0, no-unused-vars: 0, no-undef: 0*/
+'use strict';
 module.exports = {
 	initExpress: function() {
-		var xsenv = require("@sap/xsenv");
-		var passport = require("passport");
-		var xssec = require("@sap/xssec");
-		var xsHDBConn = require("@sap/hdbext");
-		var express = require("express");
+		var xsenv = require('@sap/xsenv');
+		var passport = require('passport');
+		var xssec = require('@sap/xssec');
+		var xsHDBConn = require('@sap/hdbext');
+		var express = require('express');
 		var bodyParser = require('body-parser');
 
 		//logging
-		var logging = require("@sap/logging");
+		var logging = require('@sap/logging');
 		var appContext = logging.createAppContext();
 
 		//Initialize Express App for XS UAA and HDBEXT Middleware
 		var app = express();
-		passport.use("JWT", new xssec.JWTStrategy(xsenv.getServices({
+		passport.use('JWT', new xssec.JWTStrategy(xsenv.getServices({
 			uaa: {
-				tag: "xsuaa"
+				tag: 'xsuaa'
 			}
 		}).uaa));
 		app.use(logging.expressMiddleware(appContext));
 		// app.use(bodyParser.json());
 		app.use(passport.initialize());
-		var hanaOptions = xsenv.getServices({	
+		var hanaOptions = xsenv.getServices({
 			hana: { tag: 'hana' }
 		}).hana;
 
-	app.use("/jobactivity",
+		app.use('/jobactivity',
 			xsHDBConn.middleware(hanaOptions));
-		app.use("/jobs",
-			passport.authenticate("JWT", {
+		app.use('/jobs',
+			passport.authenticate('JWT', {
 				session: false
 			}),
 			xsHDBConn.middleware(hanaOptions));
-		app.use("/schedules",
-		passport.authenticate("JWT", {
+		app.use('/schedules',
+			passport.authenticate('JWT', {
 				session: false
 			}),
-			xsHDBConn.middleware(hanaOptions));	
-		//		app.use(xsHDBConn.middleware()); 	
-		return app;
-	},
+			xsHDBConn.middleware(hanaOptions));
+			return app;
+		},
 
 	initXSJS: function(app) {
-		var xsjs = require("@sap/xsjs");
-		var xsenv = require("@sap/xsenv");
+		var xsjs = require('@sap/xsjs');
+		var xsenv = require('@sap/xsenv');
 		var options = {// anonymous : true, // remove to authenticate calls
-			redirectUrl: "/index.xsjs"
+			redirectUrl: '/index.xsjs'
 		};
 
 		//configure HANA
 		try {
 			options = Object.assign(options, xsenv.getServices({
 				hana: {
-					tag: "hana"
+					tag: 'hana'
 				}
 			}));
 		} catch (err) {
@@ -65,7 +64,7 @@ module.exports = {
 		try {
 			options = Object.assign(options, xsenv.getServices({
 				uaa: {
-					tag: "xsuaa"
+					tag: 'xsuaa'
 				}
 			}));
 		} catch (err) {
