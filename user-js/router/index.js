8f5cc55ac887050d007086ea5ed7e3298deec272
Kiruthika N <kiruthika.n@sap.com>
2017-12-17 18:47:46 +0530

    partially restructured

--- user-js/router/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint no-undef:0,no-unused-vars: 0*/
-'use strict';
-module.exports = function(app,server) {
-	app.use('/jobactivity', require('./routes/jobactivity')());
-	app.use('/jobs', require('./routes/jobs')());
-	app.use('/schedules', require('./routes/schedules')());
-};
