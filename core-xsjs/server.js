aac1bf706e73e70f47a4b65939a718d309aa1c93
Kiruthika N <kiruthika.n@sap.com>
2017-12-20 15:55:34 +0530

    Hana Secure Store Implementation

--- core-xsjs/server.js
+++ core-xsjs/server.js
@@ -6,42 +6,41 @@ var xsjs = require("@sap/xsjs");
 var xsenv = require("@sap/xsenv");
 var port = process.env.PORT || 3000;
 global.__base = __dirname + "/";
+		
+		var options = {
+			// anonymous : true, // remove to authenticate calls
+			redirectUrl: "/index.xsjs",
+			context: {
+				base: global.__base,
+				env: process.env,
+				answer: 42
+			}
+		};
+try{
 
-var options = {
-	//	anonymous : true, // remove to authenticate calls
-	redirectUrl: "/index.xsjs",
-	context: {
-		base: global.__base,
-		env: process.env,
-		answer: 42
-	}
-};
-
-// configure HANA
-try {
-	options = Object.assign(options, xsenv.getServices({
-		hana: {
-			tag: "hana"
+var op = xsenv.filterCFServices({
+						  label: 'hana',
+						  plan: 'hdi-shared'
+         })[0].credentials;
+         					
+var hana_options = { 'hana': op };
+		    
+var options =  Object.assign(options,hana_options);
+						
+var securestore_options = xsenv.filterCFServices({
+						  label: 'hana',
+						  plan: 'securestore'
+         })[0].credentials;
+         
+securestore_options = { 'secureStore': securestore_options };
+			
+options = Object.assign(options, securestore_options);
+			
+		} catch (err) {
+			console.log("[ERR]", err.message);
 		}
-	}));
-	options = Object.assign(options, xsenv.getServices({
-		secureStore: {
-			tag: "hana"
-		}
-	}));
-} catch (err) {
-	console.log("[WARN]", err.message);
-}
 
-//Add SQLCC
-try {
-	options.hana.sqlcc = xsenv.getServices({
-		"xsjs.sqlcc_config": "CROSS_SCHEMA_SFLIGHT"
-	});
-} catch (err) {
-	console.log("[WARN]", err.message);
-}
-
+		
 // configure UAA
 try {
 	options = Object.assign(options, xsenv.getServices({
@@ -55,5 +54,5 @@ try {
 
 // start server
 xsjs(options).listen(port);
-
 console.log("Server listening on port %d", port);
+
