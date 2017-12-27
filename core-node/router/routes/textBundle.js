cbe79486764632b59430a880b4139356470685b7
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-12-27 18:05:53 +0530

    [FIXED] lint errors

--- core-node/router/routes/textBundle.js
+++ core-node/router/routes/textBundle.js
@@ -1,14 +1,14 @@
 /*eslint no-console: 0, no-unused-vars: 0, consistent-return: 0, new-cap: 0*/
 /*eslint-env node, es6 */
-"use strict";
-var express = require("express");
+'use strict';
+var express = require('express');
 var app = express.Router();
-var os = require("os");
-var TextBundle = require("@sap/textbundle").TextBundle;
-var langparser = require("accept-language-parser");
+var os = require('os');
+var TextBundle = require('@sap/textbundle').TextBundle;
+var langparser = require('accept-language-parser');
 
 function getLocale(req) {
-	var lang = req.headers["accept-language"];
+	var lang = req.headers['accept-language'];
 	if (!lang) {
 		return;
 	}
@@ -18,20 +18,20 @@ function getLocale(req) {
 	}
 	var locale = arr[0].code;
 	if (arr[0].region) {
-		locale += "_" + arr[0].region;
+		locale += '_' + arr[0].region;
 	}
 	return locale;
 }
 
 module.exports = function() {
 
-	app.get("/", (req, res) => {
-		var bundle = new TextBundle(global.__base + "i18n/messages", getLocale(req));
+	app.get('/', (req, res) => {
+		var bundle = new TextBundle(global.__base + 'i18n/messages', getLocale(req));
 		res.writeHead(200, {
-			"Content-Type": "text/plain; charset=utf-8"
+			'Content-Type': 'text/plain; charset=utf-8'
 		});
-		var greeting = bundle.getText("greeting", [os.hostname(), os.type()]);
-		res.end(greeting, "utf-8");
+		var greeting = bundle.getText('greeting', [os.hostname(), os.type()]);
+		res.end(greeting, 'utf-8');
 	});
 	return app;
 };
