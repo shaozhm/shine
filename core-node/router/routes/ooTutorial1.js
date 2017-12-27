daf80ddcc066e3978a7bcafc60c962b0d61921ca
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-12-27 18:01:40 +0530

    [FIXED] lint errors

--- core-node/router/routes/ooTutorial1.js
+++ core-node/router/routes/ooTutorial1.js
@@ -1,4 +1,4 @@
-/*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0, dot-notation:0, no-use-before-define:0, no-inner-declarations:0 */
+/*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0, dot-notation:0, no-use-before-define:0, no-inner-declarations:0, no-undef:0 */
 /*eslint-env node, es6 */
 module.exports = class {
 	constructor(id) {
@@ -7,7 +7,7 @@ module.exports = class {
 	myFirstMethod(import1) {
 		if (import1 === 20) {
 			let error = {};
-			error.message = "Something Bad Happend";
+			error.message = 'Something Bad Happend';
 			error.value = import1;
 			throw error;
 		}
