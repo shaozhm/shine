346df66d9b8f0fc717a5c2ec7a2a69baf8fcfe7a
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-12-27 17:50:00 +0530

    [FIXED] lint errors

--- core-node/utils/util.js
+++ core-node/utils/util.js
@@ -1,6 +1,6 @@
 /*eslint no-console: 0, no-undef: 0, no-unused-vars: 0, no-shadow: 0, quotes: 0, no-use-before-define: 0, new-cap:0 */
-"use strict";
-var xsenv = require("@sap/xsenv");
+'use strict';
+var xsenv = require('@sap/xsenv');
 
 module.exports = {
 	callback: function(error, res, message) {
@@ -21,7 +21,7 @@ module.exports = {
 		}
 	},
 	appconfig: function() {
-        var services = xsenv.getServices({jobscheduler:{ tag: "jobscheduler" }}).jobscheduler;
+        var services = xsenv.getServices({jobscheduler:{ tag: 'jobscheduler' }}).jobscheduler;
 		return {
 			timeout: 15000,
 			user: services.user,
