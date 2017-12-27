328c524377ee5899f0c7f1c025cdb592f8d9f716
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-12-27 17:57:44 +0530

    [FIXED] lint errors

--- core-node/router/routes/exerciseAsync.js
+++ core-node/router/routes/exerciseAsync.js
@@ -1,28 +1,28 @@
 /*eslint no-console: 0, no-unused-vars: 0, new-cap:0 */
 /*eslint-env node, es6 */
-"use strict";
-var express = require("express");
-var WebSocketServer = require("ws").Server;
+'use strict';
+var express = require('express');
+var WebSocketServer = require('ws').Server;
 
 module.exports = function(server) {
 	var app = express.Router();
-	var asyncLib = require(global.__base + "async/async.js");
-	var dbAsync = require(global.__base + "async/databaseAsync.js");
-	var dbAsync2 = require(global.__base + "async/databaseAsync2.js");
-	var fileSync = require(global.__base + "async/fileSync.js");
-	var fileAsync = require(global.__base + "async/fileAsync.js");
-	var httpClient = require(global.__base + "async/httpClient.js");
+	var asyncLib = require(global.__base + 'async/async.js');
+	var dbAsync = require(global.__base + 'async/databaseAsync.js');
+	var dbAsync2 = require(global.__base + 'async/databaseAsync2.js');
+	var fileSync = require(global.__base + 'async/fileSync.js');
+	var fileAsync = require(global.__base + 'async/fileAsync.js');
+	var httpClient = require(global.__base + 'async/httpClient.js');
 
 	app.use((req, res) => {
 		var output = `<H1>Asynchronous Examples</H1></br> 
 			<a href="/exerciseAsync">/exerciseAsync</a> - Test Framework for Async Examples</br>` +
-			require(global.__base + "utils/exampleTOC").fill();
-		res.type("text/html").status(200).send(output);
+			require(global.__base + 'utils/exampleTOC').fill();
+		res.type('text/html').status(200).send(output);
 	});
 	try {
 		var wss = new WebSocketServer({
 			server: server,
-			path: "/node/excAsync",
+			path: '/node/excAsync',
 			perMessageDeflate: false
 		});
 
@@ -33,44 +33,44 @@ module.exports = function(server) {
 			wss.clients.forEach(function each(client) {
 				try {
 					client.send(message, function ack(error) {
-						if(typeof error !== "undefined") {
+						if(typeof error !== 'undefined') {
 							console.log(`Send Error: ${error.toString()}`);
 						}
 					});
 				} catch (e) {
-					console.log("Broadcast Error: %s", e.toString());
+					console.log('Broadcast Error: %s', e.toString());
 				}
 			});
-			console.log("sent: %s", message);
+			console.log('sent: %s', message);
 
 		};
-		wss.on("error", (error) => {
+		wss.on('error', (error) => {
 			console.log(`Web Socket Server Error: ${error.toString()}`);
 		});
 
-		wss.on("connection", (ws) => {
-			console.log("Connected");
+		wss.on('connection', (ws) => {
+			console.log('Connected');
 
-			ws.on("message", (message) => {
-				console.log("received: %s", message);
+			ws.on('message', (message) => {
+				console.log('received: %s', message);
 				var data = JSON.parse(message);
 				switch (data.action) {
-					case "async":
+					case 'async':
 						asyncLib.asyncDemo(wss);
 						break;
-					case "fileSync":
+					case 'fileSync':
 						fileSync.fileDemo(wss);
 						break;
-					case "fileAsync":
+					case 'fileAsync':
 						fileAsync.fileDemo(wss);
 						break;
-					case "httpClient":
+					case 'httpClient':
 						httpClient.callService(wss);
 						break;
-					case "dbAsync":
+					case 'dbAsync':
 						dbAsync.dbCall(wss);
 						break;
-					case "dbAsync2":
+					case 'dbAsync2':
 						dbAsync2.dbCall(wss);
 						break;
 					default:
@@ -78,16 +78,16 @@ module.exports = function(server) {
 						break;
 				}
 			});
-			ws.on("close", () => {
-				console.log("Closed");
+			ws.on('close', () => {
+				console.log('Closed');
 			});
-			ws.on("error", (error) => {
+			ws.on('error', (error) => {
 				console.log(`Web Socket Error: ${error.toString()}`);
 			});
 			ws.send(JSON.stringify({
-				text: "Connected to Exercise 3"
+				text: 'Connected to Exercise 3'
 			}), function ack(error) {
-				if (typeof error !== "undefined") {
+				if (typeof error !== 'undefined') {
 					console.log(`Send Error: ${error.toString()}`);
 				}
 			});
