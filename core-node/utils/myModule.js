e8c980936be838aade241a1fb5ab8a8fe5d44f4a
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-12-27 17:47:32 +0530

    [FIXED] lint errors

--- core-node/utils/myModule.js
+++ core-node/utils/myModule.js
@@ -1,7 +1,8 @@
-"use strict";
-
+/*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0, dot-notation:0, no-use-before-define:0, no-undef:0 */
+/*eslint-env node, es6 */
+'use strict';
 module.exports = {
-   helloModule: function(){
-   	return "Hello World from My First Module";
+	helloModule: function(){
+		return 'Hello World from My First Module';
    }
 };
