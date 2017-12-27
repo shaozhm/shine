4284e911d5a1ef34d6109ff5cba680dbae516690
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-12-27 17:44:24 +0530

    [FIXED] lint errors

--- core-node/async/fileSync.js
+++ core-node/async/fileSync.js
@@ -1,18 +1,13 @@
 /*eslint-env node, es6 */
-"use strict";
-var fs = require("fs");
-
+'use strict';
+var fs = require('fs');
 module.exports = {
 	fileDemo: (wss) => {
-		var text = fs.readFileSync("./async/file.txt", "utf8");
+		var text = fs.readFileSync('./async/file.txt', 'utf8');
 		wss.broadcast(text);
-
-		wss.broadcast("After First Read\n");
-
-		text = fs.readFileSync("./async/file2.txt", "utf8");
+		wss.broadcast('After First Read\n');
+		text = fs.readFileSync('./async/file2.txt', 'utf8');
 		wss.broadcast(text);
-
-		wss.broadcast("After Second Read\n");
-
+		wss.broadcast('After Second Read\n');
 	}
 };
