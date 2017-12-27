818b2f47a2c6cbc0cbb5f44ef054bccb03209553
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-12-27 18:02:58 +0530

    [FIXED] lint errors

--- core-node/router/routes/ooTutorial3.js
+++ core-node/router/routes/ooTutorial3.js
@@ -1,14 +1,14 @@
-/*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0, dot-notation:0, no-use-before-define:0, no-inner-declarations:0 */
+/*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0, dot-notation:0, no-use-before-define:0, no-inner-declarations:0, no-undef:0 */
 /*eslint-env node, es6 */
 module.exports = class {
 	constructor(db) {
 		this.db = db;
 	}
 	getFlightDetails(carrierId, connectionId, flightDate) {
-		let dbPromises = require(global.__base + "utils/dbPromises");
+		let dbPromises = require(global.__base + 'utils/dbPromises');
 		let dbConn = new dbPromises(this.db);
 		return new Promise((resolve, reject) => {
-			dbConn.preparePromisified("select * from SFLIGHT WHERE CARRID = ? AND CONNID = ? and FLDATE = ? ")
+			dbConn.preparePromisified('select * from SFLIGHT WHERE CARRID = ? AND CONNID = ? and FLDATE = ? ')
 				.then(statement => {
 					dbConn.statementExecPromisified(statement, [carrierId, connectionId, flightDate])
 						.then(results => {
@@ -16,7 +16,7 @@ module.exports = class {
 						})
 						.catch(err => {
 							let error = {};
-							error.message = "Invalid Flight";
+							error.message = 'Invalid Flight';
 							error.carrierId = carrierId;
 							error.connectionId = connectionId;
 							error.flightDate = flightDate;
@@ -30,19 +30,19 @@ module.exports = class {
 	}
 
 	calculateFlightPrice(carrierId, connectionId, flightDate) {
-		let dbPromises = require(global.__base + "utils/dbPromises");
+		let dbPromises = require(global.__base + 'utils/dbPromises');
 		let dbConn = new dbPromises(this.db);
 		return new Promise((resolve, reject) => {
-			dbConn.preparePromisified("select PRICE, CURRENCY, PLANETYPE from SFLIGHT WHERE CARRID = ? AND CONNID = ? and FLDATE = ? ")
+			dbConn.preparePromisified('select PRICE, CURRENCY, PLANETYPE from SFLIGHT WHERE CARRID = ? AND CONNID = ? and FLDATE = ? ')
 				.then(statement => {
 					dbConn.statementExecPromisified(statement, [carrierId, connectionId, flightDate])
 						.then(results => {
 							let price;
 							switch (results[0].PLANETYPE) {
-								case "747-400":
+								case '747-400':
 									price = results[0].PRICE + 40;
 									break;
-								case "A310-300":
+								case 'A310-300':
 									price = results[0].PRICE + 25;
 									break;
 								default:
@@ -53,7 +53,7 @@ module.exports = class {
 						})
 						.catch(err => {
 							let error = {};
-							error.message = "Invalid Flight";
+							error.message = 'Invalid Flight';
 							error.carrierId = carrierId;
 							error.connectionId = connectionId;
 							error.flightDate = flightDate;
