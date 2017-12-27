e047b86d836db3b318e7cb481732fed4444da472
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-12-27 18:04:10 +0530

    [FIXED] lint errors

--- core-node/router/routes/promises.js
+++ core-node/router/routes/promises.js
@@ -1,15 +1,15 @@
 /*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0, dot-notation:0, no-use-before-define:0 */
 /*eslint-env node, es6 */
 
-"use strict";
-var express = require("express");
+'use strict';
+var express = require('express');
 
 module.exports = function() {
 	var app = express.Router();
 
 	function readFilePromisified(filename) {
 		return new Promise((resolve, reject) => {
-			require("fs").readFile(filename, "utf8", (error, data) => {
+			require('fs').readFile(filename, 'utf8', (error, data) => {
 				if (error) {
 					reject(error);
 				} else {
@@ -77,23 +77,23 @@ module.exports = function() {
 	}
 
 	//Hello Router
-	app.get("/", (req, res) => {
+	app.get('/', (req, res) => {
 		var output =
 			`<H1>ES6 Promises</H1></br>
 			<a href="${req.baseUrl}/promises">/promises</a> - Manual Promisefy readFile</br>
 			<a href="${req.baseUrl}/promisesError">/promisesError</a> - Manual Promisefy readFile and catch error</br> 
 			<a href="${req.baseUrl}/promisesDB1">/promisesDB1</a> - Simple HANA DB Select via Promises</br>
 			<a href="${req.baseUrl}/promisesDB2">/promisesDB2</a> - Simple Database Call Stored Procedure via Promises</br>` +
-			require(global.__base + "utils/exampleTOC").fill();
-		res.type("text/html").status(200).send(output);
+			require(global.__base + 'utils/exampleTOC').fill();
+		res.type('text/html').status(200).send(output);
 	});
 
 	//Manual Promisefy readFile
-	app.get("/promises", function(req, res) {
-		var body = "";
-		readFilePromisified(global.__base + "async/file.txt")
+	app.get('/promises', function(req, res) {
+		var body = '';
+		readFilePromisified(global.__base + 'async/file.txt')
 			.then(text => {
-				return res.type("text/html").status(200).send(text);
+				return res.type('text/html').status(200).send(text);
 			})
 			.catch(error => {
 				console.log(error);
@@ -101,47 +101,47 @@ module.exports = function() {
 	});
 
 	//Manual Promisefy readFile and catch error
-	app.get("/promisesError", function(req, res) {
-		var body = "";
+	app.get('/promisesError', function(req, res) {
+		var body = '';
 
-		readFilePromisified(global.__base + "async/missing.txt")
+		readFilePromisified(global.__base + 'async/missing.txt')
 			.then(text => {
-				return res.type("text/html").status(200).send(text);
+				return res.type('text/html').status(200).send(text);
 			})
 			.catch(error => {
-				return res.type("text/html").status(200).send(error.toString());
+				return res.type('text/html').status(200).send(error.toString());
 			});
 	});
 
 	//Simple Database Select Promises
-	app.get("/promisesDB1", function(req, res) {
+	app.get('/promisesDB1', function(req, res) {
 		let db = new promisedDB(req.db);
-		db.preparePromisified("select SESSION_USER from \"DUMMY\"")
+		db.preparePromisified('select SESSION_USER from \"DUMMY\"')
 			.then(statement => {
 				db.statementExecPromisified(statement, [])
 					.then(results => {
 						let result = JSON.stringify({
 							Objects: results
 						});
-						res.type("application/json").status(200).send(result);
+						res.type('application/json').status(200).send(result);
 					})
 					.catch(err => {
-						res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+						res.type('text/plain').status(500).send(`ERROR: ${err.toString()}`);
 						return;
 					});
 			})
 			.catch(err => {
-				res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+				res.type('text/plain').status(500).send(`ERROR: ${err.toString()}`);
 				return;
 			});
 	});
 
 	//Simple Database Call Stored Procedure via Promises
-	app.get("/promisesDB2", function(req, res) {
+	app.get('/promisesDB2', function(req, res) {
 		let db = new promisedDB(req.db);
-		let hdbext = require("@sap/hdbext");
+		let hdbext = require('@sap/hdbext');
 		//(client, Schema, Procedure)
-		db.loadProcedurePromisified(hdbext, null, "get_po_header_data")
+		db.loadProcedurePromisified(hdbext, null, 'get_po_header_data')
 			.then(sp => {
 				//(Input Parameters, callback(errors, Output Scalar Parameters, [Output Table Parameters])
 				db.callProcedurePromisified(sp, {})
@@ -152,14 +152,14 @@ module.exports = function() {
 						let result = JSON.stringify({
 							EX_TOP_3_EMP_PO_COMBINED_CNT: results
 						});
-						res.type("application/json").status(200).send(result);
+						res.type('application/json').status(200).send(result);
 					})
 					.catch(err => {
-						res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+						res.type('text/plain').status(500).send(`ERROR: ${err.toString()}`);
 					});
 			})
 			.catch(err => {
-				res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+				res.type('text/plain').status(500).send(`ERROR: ${err.toString()}`);
 				return;
 			});
 	});
