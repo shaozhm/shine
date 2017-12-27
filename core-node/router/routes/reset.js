33c69cd76862fa71eee75ace62814f3a1381e7d5
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-12-27 18:04:43 +0530

    [FIXED] lint errors

--- core-node/router/routes/reset.js
+++ core-node/router/routes/reset.js
@@ -1,3 +1,5 @@
+/*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, quotes: 0, no-use-before-define: 0, new-cap:0, no-undef:0 */
+'use strict';
 module.exports = function() {
 var express = require('express');
 var async = require('async');
@@ -5,16 +7,16 @@ var cds = require('@sap/cds');
 //var router = express.Router();
 var winston = require('winston');
 //var util = require(global.__base + "utils/datagen");
-var util = require("./util");
+var util = require('./util');
 var app = express.Router();
 winston.level = process.env.winston_level || 'error';
 app.post('/soheader', function (req, res) {
     console.log('reset so header triggered');
     util.resetTable(
-        req, 
-        res,
-        "SO.Header", 
-        "SOShadow.Header",
+        req,
+		res,
+        'SO.Header',
+		'SOShadow.Header',
         util.callback
     );
 });
@@ -22,10 +24,10 @@ app.post('/soheader', function (req, res) {
 app.post('/soitem', function (req, res) {
     console.log('reset so item triggered');
     util.resetTable(
-        req, 
-        res,
-        "SO.Item", 
-        "SOShadow.Item",
+        req,
+		res,
+        'SO.Item',
+		'SOShadow.Item',
         util.callback
     );
 });
@@ -33,10 +35,10 @@ app.post('/soitem', function (req, res) {
 app.post('/poheader', function (req, res) {
     console.log('reset po header triggered');
     util.resetTable(
-        req, 
-        res,
-        "PO.Header", 
-        "POShadow.Header",
+        req,
+		res,
+        'PO.Header',
+		'POShadow.Header',
         util.callback
     );
 });
@@ -44,10 +46,10 @@ app.post('/poheader', function (req, res) {
 app.post('/poitem', function (req, res) {
     console.log('reset po item triggered');
     util.resetTable(
-        req, 
-        res,
-        "PO.Item", 
-        "POShadow.Item",
+        req,
+		res,
+        'PO.Item',
+		'POShadow.Item',
         util.callback
     );
 });
@@ -55,10 +57,10 @@ app.post('/poitem', function (req, res) {
 app.post('/addresses', function (req, res) {
     console.log('reset addresses triggered');
     util.resetTable(
-        req, 
-        res,
-        "MD.Addresses", 
-        "MDShadow.Addresses",
+        req,
+		res,
+        'MD.Addresses',
+		'MDShadow.Addresses',
         util.callback
     );
 });
@@ -66,10 +68,10 @@ app.post('/addresses', function (req, res) {
 app.post('/partners', function (req, res) {
     console.log('reset business partners triggered');
     util.resetTable(
-        req, 
-        res,
-        "MD.BusinessPartner", 
-        "MDShadow.BusinessPartner",
+        req,
+		res,
+        'MD.BusinessPartner',
+		'MDShadow.BusinessPartner',
         util.callback
     );
 });
@@ -77,10 +79,10 @@ app.post('/partners', function (req, res) {
 app.post('/employees', function (req, res) {
     console.log('reset employees triggered');
     util.resetTable(
-        req, 
-        res,
-        "MD.Employees", 
-        "MDShadow.Employees",
+        req,
+		res,
+        'MD.Employees',
+		'MDShadow.Employees',
         util.callback
     );
 });
@@ -88,10 +90,10 @@ app.post('/employees', function (req, res) {
 app.post('/products', function (req, res) {
     console.log('reset products triggered');
     util.resetTable(
-        req, 
-        res,
-        "MD.Products", 
-        "MDShadow.Products",
+        req,
+		res,
+        'MD.Products',
+		'MDShadow.Products',
         util.callback
     );
 });
@@ -99,10 +101,10 @@ app.post('/products', function (req, res) {
 app.post('/constants', function (req, res) {
     console.log('reset constants triggered');
     util.resetTable(
-        req, 
-        res,
-        "Util.Constants", 
-        "UtilShadow.Constants",
+        req,
+		res,
+        'Util.Constants',
+		'UtilShadow.Constants',
         util.callback
     );
 });
@@ -110,10 +112,10 @@ app.post('/constants', function (req, res) {
 app.post('/texts', function (req, res) {
     console.log('reset texts triggered');
     util.resetTable(
-        req, 
-        res,
-        "Util.Texts", 
-        "UtilShadow.Texts",
+        req,
+		res,
+        'Util.Texts',
+		'UtilShadow.Texts',
         util.callback
     );
 });
@@ -121,10 +123,10 @@ app.post('/texts', function (req, res) {
 app.post('/notes', function (req, res) {
     console.log('reset notes triggered');
     util.resetTable(
-        req, 
-        res,
-        "Util.Notes", 
-        "UtilShadow.Notes",
+        req,
+		res,
+        'Util.Notes',
+		'UtilShadow.Notes',
         util.callback
     );
 });
@@ -132,13 +134,13 @@ app.post('/notes', function (req, res) {
 app.post('/attachments', function (req, res) {
     console.log('reset attachments triggered');
     util.resetTable(
-        req, 
-        res,
-        "Util.Attachments", 
-        "UtilShadow.Attachments",
+        req,
+		res,
+        'Util.Attachments',
+		'UtilShadow.Attachments',
         util.callback
     );
 });
 
 return app;
-}
+};
