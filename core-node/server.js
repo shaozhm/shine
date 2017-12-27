6bcfd6e67d121fcd5feaf61d3297a4ec9a680995
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-12-27 17:34:33 +0530

    [FIXED] lint errors

--- core-node/server.js
+++ core-node/server.js
@@ -1,55 +1,54 @@
 /*eslint no-console: 0, no-unused-vars: 0, no-undef:0*/
 /*eslint-env node, es6 */
 
-"use strict";
-var https = require("https");
-var xsenv = require("@sap/xsenv");
+'use strict';
+var https = require('https');
 var port = process.env.PORT || 3000;
-var server = require("http").createServer();
+var server = require('http').createServer();
 https.globalAgent.options.ca = xsenv.loadCertificates();
-global.__base = __dirname + "/";
+global.__base = __dirname + '/';
 
 //Initialize Express App for XSA UAA and HDBEXT Middleware
-var xsenv = require("@sap/xsenv");
-var passport = require("passport");
-var xssec = require("@sap/xssec");
-var xsHDBConn = require("@sap/hdbext");
-var express = require("express");
+var xsenv = require('@sap/xsenv');
+var passport = require('passport');
+var xssec = require('@sap/xssec');
+var xsHDBConn = require('@sap/hdbext');
+var express = require('express');
 
 //logging
-var logging = require("@sap/logging");
+var logging = require('@sap/logging');
 var appContext = logging.createAppContext();
 
 //Initialize Express App for XS UAA and HDBEXT Middleware
 var app = express();
 
-passport.use("JWT", new xssec.JWTStrategy(xsenv.getServices({
+passport.use('JWT', new xssec.JWTStrategy(xsenv.getServices({
 	uaa: {
-		tag: "xsuaa"
+		tag: 'xsuaa'
 	}
 }).uaa));
 app.use(logging.expressMiddleware(appContext));
 app.use(passport.initialize());
 var hanaOptions = xsenv.getServices({
 	hana: {
-		tag: "hana"
+		tag: 'hana'
 	}
 });
 //hanaOptions.hana.rowsWithMetadata = true;
 app.use('/jobactivity',
 			xsHDBConn.middleware(hanaOptions.hana));
 app.use('/',
-	passport.authenticate("JWT", {
+	passport.authenticate('JWT', {
 		session: false
 	}),
 	xsHDBConn.middleware(hanaOptions.hana)
 );
 
 //Setup Routes
-var router = require("./router")(app, server);
+var router = require('./router')(app, server);
 
-//Start the Server 
-server.on("request", app);
+//Start the Server
+server.on('request', app);
 server.listen(port, function() {
 	console.info(`HTTP Server: ${server.address().port}`);
 });
