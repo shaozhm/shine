5e0a15098f3a458c741725a758cd2dd5880ce739
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-12-27 18:00:29 +0530

    [FIXED] lint errors

--- core-node/router/routes/lrep.js
+++ core-node/router/routes/lrep.js
@@ -1,36 +1,36 @@
 /*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0*/
 /*eslint-env node, es6 */
-"use strict";
-var express = require("express");
+'use strict';
+var express = require('express');
 
 function upsertVariant(req, res) {
 	var body = req.body;
 	var client = req.db;
-	var insertString = "UPSERT \"lrep.variants\" " +
-		" (\"fileName\", \"fileType\", \"changeType\", \"reference\", \"packageName\", \"content\", " +
-		"  \"namespace\", \"originalLanguage\", \"conditions\", \"context\", " +
-		"  \"supportGenerator\", \"supportService\", \"supportUser\",  " +
-		"  \"layer\", \"selector\", \"texts\", \"variantName\" ) " +
-		" VALUES(?, ?, ?, ?, ?, TO_NCLOB(?), ?, ?, ?, ?, ?, ?, SESSION_CONTEXT('APPLICATIONUSER'), ?, ?, TO_NCLOB(?), ? ) " +
-		" WHERE \"changeType\" = ? AND \"fileType\" = ? AND \"layer\" = ? AND \"supportUser\" = SESSION_CONTEXT('APPLICATIONUSER') " +
-		" AND \"variantName\" = ? ";
+	var insertString = 'UPSERT \"lrep.variants\" ' +
+		' (\"fileName\", \"fileType\", \"changeType\", \"reference\", \"packageName\", \"content\", '+
+		'  \"namespace\", \"originalLanguage\", \"conditions\", \"context\", '+
+		'  \"supportGenerator\", \"supportService\", \"supportUser\",  ' +
+		'  \"layer\", \"selector\", \"texts\", \"variantName\" ) ' +
+		' VALUES(?, ?, ?, ?, ?, TO_NCLOB(?), ?, ?, ?, ?, ?, ?, SESSION_CONTEXT("APPLICATIONUSER"), ?, ?, TO_NCLOB(?), ? ) ' +
+		' WHERE \"changeType\" = ? AND \"fileType\" = ? AND \"layer\" = ? AND \"supportUser\" = SESSION_CONTEXT("APPLICATIONUSER") ' +
+		' AND \"variantName\" = ? ';
 	client.prepare(
 		insertString,
 		(err, statement) => {
 			if (err) {
-				res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+				res.type('text/plain').status(500).send(`ERROR: ${err.toString()}`);
 				return;
 			}
-			var generator = "";
-			var service = "";
-			var variantName = "";
+			var generator = '';
+			var service = '';
+			var variantName = '';
 
-			if (typeof body.support !== "undefined") {
+			if (typeof body.support !== 'undefined') {
 				generator = body.support.generator;
 				service = body.support.service;
 			}
-			if (typeof body.texts !== "undefined") {
-				if (typeof body.texts.variantName !== "undefined") {
+			if (typeof body.texts !== 'undefined') {
+				if (typeof body.texts.variantName !== 'undefined') {
 					variantName = body.texts.variantName.value;
 				}
 			}
@@ -42,10 +42,10 @@ function upsertVariant(req, res) {
 				],
 				(err, results) => {
 					if (err) {
-						res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+						res.type('text/plain').status(500).send(`ERROR: ${err.toString()}`);
 						return;
 					} else {
-						res.type("application/json").status(200).send(body);
+						res.type('application/json').status(200).send(body);
 					}
 				});
 		});
@@ -54,50 +54,50 @@ function upsertVariant(req, res) {
 
 module.exports = function() {
 	var app = express.Router();
-	var bodyParser = require("body-parser");
+	var bodyParser = require('body-parser');
 	app.use(bodyParser.json());
 
-	app.get("/", (req, res) => {
-		res.type("text/html").status(200).send("");
+	app.get('/', (req, res) => {
+		res.type('text/html').status(200).send('');
 	});
 
-	app.get("/actions/getcsrftoken/", (req, res) => {
-		res.type("text/html").status(200).send("");
+	app.get('/actions/getcsrftoken/', (req, res) => {
+		res.type('text/html').status(200).send('');
 	});
 
-	app.post("/variants/", (req, res) => {
+	app.post('/variants/', (req, res) => {
 		upsertVariant(req, res);
 	});
 
-	app.put("/variants/:fileName", (req, res) => {
+	app.put('/variants/:fileName', (req, res) => {
 		var body = req.body;
 		var client = req.db;
 		var fileNameInput = req.params.fileName;
 
-		var insertString = "UPSERT \"lrep.variants\" " +
-			" (\"fileName\", \"fileType\", \"changeType\", \"reference\", \"packageName\", \"content\", " +
-			"  \"namespace\", \"originalLanguage\", \"conditions\", \"context\", " +
-			"  \"supportGenerator\", \"supportService\", \"supportUser\",  " +
-			"  \"layer\", \"selector\", \"texts\", \"variantName\" ) " +
-			" VALUES(?, ?, ?, ?, ?, TO_NCLOB(?), ?, ?, ?, ?, ?, ?, SESSION_CONTEXT('APPLICATIONUSER'), ?, ?, TO_NCLOB(?), ? ) " +
-			" WHERE \"fileName\" = ? ";
+		var insertString = 'UPSERT \"lrep.variants\" ' +
+			' (\"fileName\", \"fileType\", \"changeType\", \"reference\", \"packageName\", \"content\", ' +
+			'  \"namespace\", \"originalLanguage\", \"conditions\", \"context\", ' +
+			'  \"supportGenerator\", \"supportService\", \"supportUser\",  ' +
+			'  \"layer\", \"selector\", \"texts\", \"variantName\" ) ' +
+			' VALUES(?, ?, ?, ?, ?, TO_NCLOB(?), ?, ?, ?, ?, ?, ?, SESSION_CONTEXT("APPLICATIONUSER"), ?, ?, TO_NCLOB(?), ? ) ' +
+			' WHERE \"fileName\" = ? ';
 		client.prepare(
 			insertString,
 			(err, statement) => {
 				if (err) {
-					res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+					res.type('text/plain').status(500).send(`ERROR: ${err.toString()}`);
 					return;
 				}
-				var generator = "";
-				var service = "";
-				var variantName = "";
+				var generator = '';
+				var service = '';
+				var variantName = '';
 
-				if (typeof body.support !== "undefined") {
+				if (typeof body.support !== 'undefined') {
 					generator = body.support.generator;
 					service = body.support.service;
 				}
-				if (typeof body.texts !== "undefined") {
-					if (typeof body.texts.variantName !== "undefined") {
+				if (typeof body.texts !== 'undefined') {
+					if (typeof body.texts.variantName !== 'undefined') {
 						variantName = body.texts.variantName.value;
 					}
 				}
@@ -109,72 +109,72 @@ module.exports = function() {
 					],
 					(err, results) => {
 						if (err) {
-							res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+							res.type('text/plain').status(500).send(`ERROR: ${err.toString()}`);
 							return;
 						} else {
-							res.type("application/json").status(200).send(body);
+							res.type('application/json').status(200).send(body);
 						}
 					});
 			});
 
 	});
 
-	app.delete("/variants/:fileName", (req, res) => {
+	app.delete('/variants/:fileName', (req, res) => {
 		var body = req.body;
 		var client = req.db;
 		var fileNameInput = req.params.fileName;
 
-		var deleteString = "DELETE FROM \"lrep.variants\" " +
-			" WHERE \"fileName\" = ? ";
+		var deleteString = 'DELETE FROM \"lrep.variants\" ' +
+			' WHERE \"fileName\" = ? ';
 		client.prepare(
 			deleteString,
 			(err, statement) => {
 				if (err) {
-					res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+					res.type('text/plain').status(500).send(`ERROR: ${err.toString()}`);
 					return;
 				}
 				statement.exec([ fileNameInput
 					],
 					(err, results) => {
 						if (err) {
-							res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+							res.type('text/plain').status(500).send(`ERROR: ${err.toString()}`);
 							return;
 						} else {
-							res.type("application/json").status(200).send(body);
+							res.type('application/json').status(200).send(body);
 						}
 					});
 			});
 
 	});
 
-	app.post("/changes/", (req, res) => {
+	app.post('/changes/', (req, res) => {
 		upsertVariant(req, res);
 	});
 
-	app.get("/flex/data/:app?", (req, res) => {
+	app.get('/flex/data/:app?', (req, res) => {
 		var outer = {
-			"changes": [],
-			"settings": {
-				"isKeyUser": true,
-				"isAtoAvailable": false,
-				"isProductiveSystem": false
+			'changes': [],
+			'settings': {
+				'isKeyUser': true,
+				'isAtoAvailable': false,
+				'isProductiveSystem': false
 			}
 		};
 		var appInput = req.params.app;
 		var client = req.db;
-		var insertString = "SELECT * from \"lrep.userVariants\" " +
-			" WHERE \"reference\" = ? ";
+		var insertString = 'SELECT * from \"lrep.userVariants\" ' +
+			' WHERE \"reference\" = ? ';
 		client.prepare(
 			insertString,
 			(err, statement) => {
 				if (err) {
-					res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+					res.type('text/plain').status(500).send(`ERROR: ${err.toString()}`);
 					return;
 				}
 				statement.exec([appInput],
 					(err, results) => {
 						if (err) {
-							res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+							res.type('text/plain').status(500).send(`ERROR: ${err.toString()}`);
 							return;
 						} else {
 							var body = {};
@@ -200,7 +200,7 @@ module.exports = function() {
 								body.support.user = results[i].user;
 								outer.changes.push(body);
 							}
-							res.type("application/json").status(200).send(outer);
+							res.type('application/json').status(200).send(outer);
 						}
 					});
 			});
