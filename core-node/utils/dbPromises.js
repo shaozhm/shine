4e5183edf6369d3ce3a26ae0355b7bc166c319c1
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-12-27 17:46:22 +0530

    [FIXED] lint errors

--- core-node/utils/dbPromises.js
+++ core-node/utils/dbPromises.js
@@ -1,6 +1,6 @@
 /*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0, dot-notation:0, no-use-before-define:0 */
 /*eslint-env node, es6 */
-"use strict";
+'use strict';
 
 module.exports = class {
 	constructor(client) {
