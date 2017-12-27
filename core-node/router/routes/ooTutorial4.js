568b8e26aa2131662d121f9ea8a238e8b0ebf1c6
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2017-12-27 18:03:35 +0530

    [FIXED] lint errors

--- core-node/router/routes/ooTutorial4.js
+++ core-node/router/routes/ooTutorial4.js
@@ -1,6 +1,6 @@
 /*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0, dot-notation:0, no-use-before-define:0, no-inner-declarations:0 */
 /*eslint-env node, es6 */
-var ooTutorial3 = require("./ooTutorial3");
+var ooTutorial3 = require('./ooTutorial3');
 module.exports = class extends ooTutorial3 {
 	constructor(db) {
 		super(db);
