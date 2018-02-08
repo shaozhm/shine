eb334f9332ebde7ceac70e5cb5fd8ee7352041ab
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2018-02-08 11:16:46 +0530

    Update index.js

--- tests/router/index.js
+++ tests/router/index.js
@@ -2,16 +2,12 @@
 var path = require('path');
 module.exports = function(app, server){
 	app.use("/integrationTestResult", require("./routes/runTest")());
-// 	app.get("/assets/app.js", function(req, res) {
-// 		res.sendFile(path.join(__dirname+'/mochawesome-report/assets/app.js'));
-//     });
-//     app.get("/assets/app.css", function(req, res) {
-// 		res.sendFile(path.join(__dirname+'/mochawesome-report/assets/app.css'));
-//     });
+	app.get("/assets/app.js", function(req, res) {
+		res.sendFile(path.join(__dirname+'/mochawesome-report/assets/app.js'));
+    });
+    app.get("/assets/app.css", function(req, res) {
+		res.sendFile(path.join(__dirname+'/mochawesome-report/assets/app.css'));
+    });
 
-//	app.use("/assets/app.js", require("./routes/runTest")());
-//	app.use("/assets/app.css", require("./routes/runTest")());
-	
-//	GET https://mo-c6d742ef0.mo.sap.corp:51012/assets/app.js net::ERR_ABORTED
 };
 
