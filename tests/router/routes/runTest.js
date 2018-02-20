47184b7d76625dfb5c5f8a530c87930dce79cd0b
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2018-02-20 15:57:35 +0530

    Update runTest.js

--- tests/router/routes/runTest.js
+++ tests/router/routes/runTest.js
@@ -3,15 +3,46 @@
 var express = require("express");
 var path    = require("path");
 var fs		= require("fs");
+var Mocha = require('mocha');
+var results = require('./../../utils/results.js');
 
 module.exports = function() {
 	var app = express.Router();
 
-	//Hello Router
 	app.get("/", function(req, res) {
-	var results = require('./../../utils/results.js');
-		res.send(results.test);
-        res.end();
+		
+		// Instantiate a Mocha instance.
+		var mocha = new Mocha({
+		  reporter: 'json'/*,
+		  reporterOptions: {
+		    reportFilename: 'Report',
+		    quiet: true,
+		    html:true
+		  }*/
+		});
+		var testDir = __dirname+"/../../utils/test";
+		
+		// Add each .js file to the mocha instance
+		fs.readdirSync(testDir).filter(function(file){
+		    // Only keep the .js files
+		    return file.substr(-3) === '.js';
+
+		}).forEach(function(file){
+		    mocha.addFile(
+			path.join(testDir, file)
+		    );
+		});
+				
+		mocha.run().on('end', function() {
+		    results.test=this.testResults;
+		});
+		
+		
+		
+		
+		var results = require('./../../utils/results.js');
+			res.send(results.test);
+		res.end();
 	});
 	return app;
 };
