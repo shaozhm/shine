e1b20789afa7559e0f6f0c7bc85f3ac4abd98f1b
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2018-02-20 15:53:19 +0530

    Update initialize.js

--- tests/utils/initialize.js
+++ tests/utils/initialize.js
@@ -60,33 +60,7 @@ module.exports = {
 				
 				
 				
-				// Instantiate a Mocha instance.
-				var mocha = new Mocha({
-				  reporter: 'json'/*,
-				  reporterOptions: {
-				    reportFilename: 'Report',
-				    quiet: true,
-				    html:true
-				  }*/
-				});
-			
-
-				var testDir = __dirname+"/test";
 				
-				// Add each .js file to the mocha instance
-				fs.readdirSync(testDir).filter(function(file){
-				    // Only keep the .js files
-				    return file.substr(-3) === '.js';
-				
-				}).forEach(function(file){
-				    mocha.addFile(
-				        path.join(testDir, file)
-				    );
-				});
-				
-				mocha.run().on('end', function() {
-				    results.test=this.testResults;
-				});
 		  	});
 
 	}
