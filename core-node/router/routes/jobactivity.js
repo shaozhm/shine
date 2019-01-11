71631b8339413f2ef52e8c5160edbd796181dac9
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2019-01-11 11:06:47 +0530

    Update jobactivity.js

--- core-node/router/routes/jobactivity.js
+++ core-node/router/routes/jobactivity.js
@@ -2,7 +2,7 @@
 /*eslint-env node, es6*/
 'use strict';
 var express = require('express');
-var JobSchedulerDB = require('./JobSchedulerDBPromises');
+//var JobSchedulerDB = require('./JobSchedulerDBPromises');
 
 module.exports = function() {
 	var app = express.Router();
@@ -50,7 +50,50 @@ module.exports = function() {
 			}
 
 			if (securityContext.checkScope(SCOPE)) {
-				var js = new JobSchedulerDB(req);
+				const dbClass = require(global.__base + "utils/dbPromises");
+				let db = new dbClass(req.db);
+				var query = 'select "jobId".NEXTVAL as nJobId from "DUMMY"';
+				
+				db.preparePromisified(query)
+				.then(statement => {
+					db.statementExecPromisified(statement, [])
+					.then(results => {
+						jobid = results[0].NJOBID;
+						timestamp = new Date().toISOString();
+						//create job
+						var query = "INSERT INTO \"Jobs.Data\" VALUES('" + jobid.toString() + "','" + jname + "','" + timestamp + "')";
+						db.preparePromisified(query)
+						.then(statement => {
+							db.statementExecPromisified(statement, [])
+							.then(results => {
+								console.log("Record inserted .....");
+								res.status(200).json({status: 'record inserted into shine'});
+							})
+							.catch((error) => {
+								logger.error('Error occured' + error);
+								res.status(500).json({message: 'error in create job, could not insert data into job table'});
+							})
+						})
+						.catch((error) => {
+							logger.error('Error occured' + error);
+							res.status(500).json({message: 'error in prepare statement'});
+						})
+						
+						
+					})
+					.catch((error) => {
+						logger.error('Error occured' + error);
+						res.status(500).json({message: 'couldnt get New JobID'});
+					})
+				})
+				.catch((error) => {
+					logger.error('Error occured' + error);
+					res.status(500).json({message: 'error in prepare statement'});
+				})
+				
+				
+				
+				/*var js = new JobSchedulerDB(req);
 				js.getNewJobId()
 				.then((rows) => {
 					jobid = rows[0].NJOBID;
@@ -76,7 +119,7 @@ module.exports = function() {
 				.catch((error) => {
 					js.closeDB();
 					logger.error('Error occured' + error);
-				});
+				});*/
 			} else {
 				logger.error('Unauthorized, Scope required is missing');
 				res.status(401).json({message: 'Unauthorized, Scope required is missing'});
