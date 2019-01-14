be3ca4ea0017547451a20c7dd77cf11fc5dad07a
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2019-01-14 14:11:39 +0530

    Update server.js

--- core-node/server.js
+++ core-node/server.js
@@ -36,8 +36,7 @@ var hanaOptions = xsenv.getServices({
 });
 //hanaOptions.hana.rowsWithMetadata = true;
 hanaOptions.hana.pooling = true;
-app.use('/jobactivity',
-			xsHDBConn.middleware(hanaOptions.hana));
+//app.use('/jobactivity', xsHDBConn.middleware(hanaOptions.hana));
 app.use('/',
 	passport.authenticate('JWT', {
 		session: false
