135efe11edf9c0d7eb5ab654ba43b306063b14cd
Bismay Kumar Biswal <bismay.kumar.biswal@sap.com>
2019-01-14 14:10:59 +0530

    Update jobactivity.js

--- core-node/router/routes/jobactivity.js
+++ core-node/router/routes/jobactivity.js
@@ -49,33 +49,10 @@ module.exports = function() {
 				return;
 			}
 
-			if (securityContext.checkScope(SCOPE)) {
-				var client = req.db;
-				client.exec(query, function(error, rows) {
-						if (error) {
-							logger.error('Error occured' + error);
-						} else {
-							jobid = rows[0].NJOBID;
-							timestamp = new Date().toISOString();
-							query = "INSERT INTO \"Jobs.Data\" VALUES('" + jobid.toString() + "','" + jname + "','" + timestamp +
-								"')";
-							client.exec(query, function(error, status) {
-								if (error) {
-									logger.error('Error occured' + error);
-									res.status(401).json({message: 'couldnt insert record to SHINE'});
-
-								} else {
-									logger.info("+++ Record inserted into Jobs.Data table +++");
-									res.status(200).json({status: 'record inserted into shine'});
-								}
-							});
-
-						}
-					});
-			
-			
-				/*const dbClass = require(global.__base + "utils/dbPromises");
+			if (securityContext.checkScope(SCOPE)) {			
+				const dbClass = require(global.__base + "utils/dbPromises");
 				let db = new dbClass(req.db);
+				
 				var query = 'select "jobId".NEXTVAL as nJobId from "DUMMY"';
 				
 				db.preparePromisified(query)
@@ -90,7 +67,7 @@ module.exports = function() {
 						.then(statement => {
 							db.statementExecPromisified(statement, [])
 							.then(results => {
-								console.log("Record inserted .....");
+								console.log("++++ Record inserted .....");
 								res.status(200).json({status: 'record inserted into shine'});
 							})
 							.catch((error) => {
@@ -113,7 +90,7 @@ module.exports = function() {
 				.catch((error) => {
 					logger.error('Error occured' + error);
 					res.status(500).json({message: 'error in prepare statement'});
-				})*/
+				})
 				
 				
 				
